0661226ce8d2f1bdcc68d80175891b4e958dc32f
Nemes <sangawenemes@gmail.com>
2023-11-27 11:59:13 +0300

    first commit

---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+
+import NewMeetupForm from "../components/Meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  const navigate = useNavigate();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-84275-default-rtdb.firebaseio.com/Meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      navigate("/");
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
